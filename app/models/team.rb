843d38a4c1491095f7c5dad5f6d2abbb7d540c12
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-03-31 17:01:48 -0600

    Add player generation and validations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,22 @@
 class Team < ApplicationRecord
   has_secure_password
+  after_create :generate_players
+
+  validates :name, uniqueness: true
+  validates :email, uniqueness: true
 
   has_many :players
+
+  def generate_players
+    10.times do
+      self.players.create(
+        name: Faker::Name.unique.name,
+        player_id: (Faker::Alphanumeric.alpha(4).upcase + rand(99).to_s),
+        speed: 1,
+        strength: 1,
+        agility: 1
+      )
+    end
+    binding.pry
+  end
 end
