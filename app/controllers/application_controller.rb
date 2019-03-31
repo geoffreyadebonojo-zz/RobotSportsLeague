f8e78e8fdbb7f5ea69274beee8648235e56067bf
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-03-30 18:14:27 -0600

    Add current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= Team.find(session[:team_id]) if session[:team_id]
+  end
+
 end
