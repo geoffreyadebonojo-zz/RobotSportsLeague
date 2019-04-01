9437096c4cff613726b36ed0b3d50d1b2d64d059
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-04-01 14:17:15 -0600

    Sort Players in pool to have team members at top

--- app/controllers/player_pools_controller.rb
+++ app/controllers/player_pools_controller.rb
@@ -1,7 +1,7 @@
 class PlayerPoolsController < ApplicationController
 
   def index
-    @available_players = current_user.players
+    @available_players = current_user.players.order(on_team: :desc)
     @players_on_team = current_user.roster.count
   end
 
