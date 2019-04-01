70c77ad30746016c1ca26c7f3028e5841e7f5256
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-04-01 14:51:06 -0600

    Add index method to Roster, responds to params

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -1,7 +1,14 @@
 class RostersController < ApplicationController
 
   def index
-    @roster = current_user.roster
+    @roster = current_user.roster.order(:id)
+  end
+
+  def edit
+    if params[:cmd]
+      @roster = current_user.auto_select_players
+      redirect_to roster_path
+    end
   end
 
 end
