264155bd2af8dc433cc17188770835fb83d465ad
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-04-01 14:17:47 -0600

    Add condition for on_team and status params

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,10 +2,20 @@ class PlayersController < ApplicationController
 
   def edit
     @player = current_user.players.where("player_id = ?", params[:player_id]).first
-    @player.on_team = params[:on_team]
-    @player.status = "unassigned"
-    @player.save
-    redirect_to pool_path
+    
+    if params[:on_team]
+      @player.on_team = params[:on_team]
+      @player.status = "unassigned"
+      @player.save
+      redirect_to pool_path
+    end
+
+    if params[:status]
+      @player.status = params[:status]
+      @player.save
+      redirect_to roster_path
+    end
+
   end
 
 end
