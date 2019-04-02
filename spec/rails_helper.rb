ec7689d5e673e07dc5fd9ba5336f3b8765ef7b91
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-04-02 14:47:39 -0600

    include Capybara DSL

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,6 +70,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include Capybara::DSL
+
   config.before(:each) do
     DatabaseCleaner.clean
   end
