18b838d0ef9e9f3cedfc155b8549aa71839fd303
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-04-01 20:11:59 -0600

    Add test for Player stats_total method

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe Player, type: :model do
       team = Team.create!(name: "A", email: "B", password: "C")
       player = team.players.create(
         name: "George Jetson",
-        strength: 5,
         speed: 10,
+        strength: 5,
         agility: 15,
         player_id: "AAAA24"
       )
@@ -29,6 +29,24 @@ RSpec.describe Player, type: :model do
     it "belongs_to team" do
       player = Player.reflect_on_association(:team)
       expect(player.macro).to eq(:belongs_to)
+    end
+  end
+
+  describe "methods" do
+    it ".stats_total" do
+      team = Team.create!(name: "A", email: "B", password: "C")
+      player = team.players.create(
+        name: "George Jetson",
+        speed: 35,
+        strength: 45,
+        agility: 30,
+        player_id: "AAAA24"
+      )
+
+      expect(player.speed).to eq(25)
+      expect(player.strength).to eq(35)
+      expect(player.agility).to eq(20)
+      expect(player.stats_total).to eq(80)
     end
   end
 end
