68fe818a36ca6baee53ef2576dd4acb4cfbeaf73
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-04-03 08:33:18 -0600

    Add feature test to unassign players on Roster page

--- spec/features/roster_spec.rb
+++ spec/features/roster_spec.rb
@@ -26,32 +26,54 @@ RSpec.describe "Roster Page" do
   it "allows user to pick starters/alternates for roster" do
 
     visit pool_path
+    expect(page).to have_content("Remaining Slots: 15")
+    expect(page).to have_content("Available Players: 100")
 
     click_link("Select Player", match: :first)
 
     visit roster_path
+    expect(current_path).to eq(roster_path)
 
     expect(Team.first.roster.count).to eq(1)
 
+    expect(page).to have_content("Starter Slots Remaining: 10")
+    expect(page).to have_content("Alternate Slots Remaining: 5")
     expect(page).to have_content("unassigned")
     expect(page).to_not have_content("starter")
     expect(page).to_not have_content("alternate")
-    expect(page).to have_content("Starter")
-    expect(page).to have_content("Alternate")
+    expect(page).to have_content("Switch to Starter")
+    expect(page).to have_content("Switch to Alternate")
 
-    click_link("Starter")
+    click_link("Switch to Starter")
 
+    expect(page).to have_content("Starter Slots Remaining: 9")
+    expect(page).to have_content("Alternate Slots Remaining: 5")
     expect(page).to_not have_content("unassigned")
-    expect(page).to_not have_content("Starter")
+    expect(page).to_not have_content("Swith to Starter")
     expect(page).to have_content("starter")
-    expect(page).to have_content("Alternate")
+    expect(page).to have_content("Switch to Alternate")
+    expect(page).to have_content("Unassign Player")
 
-    click_link("Alternate")
+    click_link("Switch to Alternate")
 
+    expect(page).to have_content("Starter Slots Remaining: 10")
+    expect(page).to have_content("Alternate Slots Remaining: 4")
     expect(page).to_not have_content("unassigned")
-    expect(page).to_not have_content("Alternate")
+    expect(page).to_not have_content("Switch to Alternate")
     expect(page).to have_content("alternate")
-    expect(page).to have_content("Starter")
+    expect(page).to have_content("Switch to Starter")
+    expect(page).to have_content("Unassign Player")
+
+    click_link("Unassign Player")
+
+    expect(page).to have_content("Starter Slots Remaining: 10")
+    expect(page).to have_content("Alternate Slots Remaining: 5")
+    expect(page).to have_content("unassigned")
+    expect(page).to have_content("Switch to Starter")
+    expect(page).to have_content("Switch to Alternate")
+    expect(page).to_not have_content("alternate")
+    expect(page).to_not have_content("starter")
+    expect(page).to_not have_content("Unassign Player")
   end
 
 end
